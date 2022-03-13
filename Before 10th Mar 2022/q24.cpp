c49eebe899bd8a7d08c5c6d0bd19b48e95f1978e
Udayan Das <89590597+uday-das@users.noreply.github.com>
2022-03-13 18:33:23 +0530

    Added 1 file: Delete alternate characters

---
+++ Before 10th Mar 2022/q24.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        string str = "";
+        for(int i = 0; i < S.length(); i++)
+        {
+            if(i % 2 == 0)
+                str.push_back(S[i]);
+        }
+        return str;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
