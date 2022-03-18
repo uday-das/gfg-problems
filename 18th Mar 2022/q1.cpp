2259ec1bc52eaedd2e49388657c1e602cde39ad7
Udayan <udayandas272013@gmail.com>
2022-03-18 12:18:37 +0530

    Add 3 files

---
+++ 18th Mar 2022/q1.cpp
@@ -0,0 +1,61 @@
+// 1. Pairing elements
+
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+array_of_Pairs(arr, N): function which returns the array of pairs
+arr[]: contains elements of given array
+N: sze of array
+*/
+vector<pair<int, int>> array_of_Pairs(int arr[], int N)
+{
+    vector <pair <int, int>> v;
+    for(int i = 0, j = N-1; i <= j; i++, j--)
+    {
+        v.push_back(make_pair(arr[i], arr[j]));
+    }
+    return v;
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int N;
+        
+        // Input size of array
+        cin>>N;
+        
+        int arr[N];
+        // Input array elements 
+        for(int i = 0; i < N; i++)
+            cin>>arr[i];
+        
+        // Store the result in vector of pair
+        vector<pair<int, int>> res = array_of_Pairs(arr, N);
+        
+        for(int i = 0; i < res.size(); i++)
+        {
+            cout << "("<< res[i].first << "," <<res[i].second << ") ";
+        }
+        cout << endl;
+        
+    }
+    return 0;
+}
