d38da8d08ac00e0eadcb1a85b5e6e6f3133ccb14
Udayan <udayandas272013@gmail.com>
2022-03-20 00:30:24 +0530

    Add 2 files

---
+++ 19th Mar 2022/q1.cpp
@@ -0,0 +1,59 @@
+// 1. Maximum Occuring Character
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the maximum occurring character in a string.
+    char getMaxOccuringChar(string str)
+    {
+        map <char, int> mp;
+        int max_freq = INT_MIN; 
+        string a = "";
+        for(int i = 0; i < str.length(); i++)
+        {
+            mp[str[i]]++;
+        }
+        map <char, int> :: iterator i;
+        for(i = mp.begin(); i != mp.end(); i++)
+        {
+            if(((*i).second) > max_freq)
+            {
+                max_freq = ((*i).second);
+            }
+        }
+        
+        int j = 0;
+        for(i = mp.begin(); i != mp.end(); i++)
+        {
+            if(((*i).second) == max_freq)
+            {
+                a[j++] = ((*i).first);
+            }
+        }
+        sort(a.begin(), a.end());
+        return a[0];
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+    	Solution obj;
+        cout<< obj.getMaxOccuringChar(str)<<endl;
+    }
+}  // } Driver Code Ends
