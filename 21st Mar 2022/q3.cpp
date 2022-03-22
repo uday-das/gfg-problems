877e473cfc378817bf4ff0aa4f2f6f8da4ccc560
Udayan <udayandas272013@gmail.com>
2022-03-22 10:56:07 +0530

    Add 2 folders

---
+++ 21st Mar 2022/q3.cpp
@@ -0,0 +1,61 @@
+// 3a. Vector Sum (Method I)
+
+// { Driver Code Starts
+//Initial Template for C++
+
+// Find sum of the vector
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete the function
+int SumMe(vector<int> arr, int n){
+    int sum = 0;
+    for(auto i : arr)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+
+    while(t--){
+
+    int n;
+    cin>>n;
+    
+    vector<int> arr(n);
+    for(int i=0; i<n;++i){
+        cin>>arr[i];
+    }
+
+    cout<<SumMe(arr, n)<<endl;
+
+    }
+
+	return 0; 
+} 
+  // } Driver Code Ends
+
+// 3b. Vector Sum (Method II)
+
+// int SumMe(vector<int> arr, int n){
+//     int sum = 0;
+//     vector <int> :: iterator i; 
+//     for(i = arr.begin(); i != arr.end(); i++)
+//     {
+//         sum += *(i);
+//     }
+//     return sum;
+// }
