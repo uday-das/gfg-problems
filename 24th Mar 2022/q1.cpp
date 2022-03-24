5c56718e9dfc9cc304b00b605897043eaaec9185
Udayan <udayandas272013@gmail.com>
2022-03-25 00:36:12 +0530

    Add 4 files

---
+++ 24th Mar 2022/q1.cpp
@@ -0,0 +1,56 @@
+// 1. Vector Shrink To Fit
+
+// { Driver Code Starts
+//Initial Template for C++
+
+// vector::shrink_to_fit
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+//Complete the function
+void ShrinkMe(vector<int> myvector, int newsize){
+
+    // Use shrink_to_fit function
+    
+    myvector.resize(newsize);
+    cout<<myvector.capacity();
+    
+    cout<<endl;
+    
+	myvector.shrink_to_fit();
+    cout<<myvector.capacity();
+
+}
+
+
+// { Driver Code Starts.
+
+int main ()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int size;
+        cin>>size;
+
+        int newsize;
+        cin>>newsize;
+
+        vector<int> myvector(size);
+
+        ShrinkMe(myvector, newsize);
+        cout<<endl;
+    
+    }
+
+  return 0;
+}
+  // } Driver Code Ends
