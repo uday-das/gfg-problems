3d6dfe9d8f11338245f74e2dc61eafee9b648a62
Udayan Das <89590597+uday-das@users.noreply.github.com>
2022-04-08 02:13:33 +0530

    Added 1 file: Deque deletion (STL)

--- 7th Apr 2022/q5.cpp
+++ 7th Apr 2022/q5.cpp
@@ -6,8 +6,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
+// } Driver Code Ends
 //User function Template for C++
 
 //Function to erase the element from specified position X in deque.
