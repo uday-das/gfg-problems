55d04aa7f6f6c979051834aa58811753418e8e5d
Udayan <udayandas272013@gmail.com>
2022-04-15 14:23:45 +0530

    Add 2 files

---
+++ April/15th Apr 2022/q1.cpp
@@ -0,0 +1,52 @@
+// 1. Maximize Number of Toys
+
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Your code here
+    int max_toys(int arr[], int n, int k)
+    {
+        priority_queue <int, vector <int>, greater <int>> pq(arr, arr + n);                 // making min heap from given array
+        int res = 0;
+        while(k >= 0 && !pq.empty() && k >= pq.top())
+        {
+            k = k - pq.top();
+            pq.pop();
+            res++;
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<< ob.max_toys(arr, n, k)<<endl;
+    }
+    return 0;
+}
+
+
+
+  // } Driver Code Ends
---
+++ April/15th Apr 2022/q2.cpp
@@ -0,0 +1,46 @@
+// 2. Kth smallest element
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the kth smallest element in the array.
+    int kthSmallest(int arr[], int n, int k)
+    {
+        priority_queue <int, vector <int>, greater <int>> pq(arr, arr + n);
+        for(int i = 1; i <= k-1; i++)
+        {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+
+// { Driver Code Starts.
+int main(){
+	
+	int t;
+	cin >> t;
+	while(t--){
+	    
+	    int n, k;
+	    cin >> n >> k;
+	    int arr[n];
+	    
+	    for(int i = 0; i<n; ++i)
+	        cin>>arr[i];
+	        
+	    Solution ob;
+	    cout << ob.kthSmallest(arr, n, k) << endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
