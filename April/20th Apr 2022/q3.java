3ac45e33d5b367c8b5e53a57efddf1029a5bd995
Udayan Das <89590597+uday-das@users.noreply.github.com>
2022-04-22 02:17:40 +0530

    Added file: Fibonacci Number

--- April/20th Apr 2022/q3.java
+++ April/20th Apr 2022/q3.java
@@ -14,7 +14,7 @@ public static int fibonacci(int n){
     if(n == 1 || n == 2)
         return 1;
     else
-        return fibonacci(n-1) + fibonacci(n-2);  
+        return fibonacci(n-1) + fibonacci(n-2);
 }
 
 // { Driver Code Starts.
