e3ec8f3467f11f5bf22ad38d7d55119566483504
Udayan <udayandas272013@gmail.com>
2022-05-21 20:27:49 +0530

    added 2 bit magic codes and 2 vec codes

---
+++ Bit Magic/num_of_one_bits.cpp
@@ -0,0 +1,36 @@
+// Number of 1 Bits
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {    
+        // Brian Kernighan's algo
+        int res = 0;
+        while(N > 0)
+        {
+            N = (N & N-1);
+            res++;
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
