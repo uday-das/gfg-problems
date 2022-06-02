dc908b61364935dd90ae2e7273f04f6d2c07e3b4
Udayan <udayandas272013@gmail.com>
2022-06-02 22:25:00 +0530

    Added 5 codes

---
+++ Arrays and misc/min_adj_diff_c_arr.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    //Function to find minimum adjacent difference in a circular array.
+    // arr[]: input array
+    // n: size of array
+    int minAdjDiff(int arr[], int n){    
+        int ans = abs(arr[n-1] - arr[0]);           // finding abs diff bet last an first el
+        for(int i = 0; i < n-1; i++)
+        {
+            ans = min(ans, abs(arr[i] - arr[i+1]));
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; //Input testcases
+    
+    while(t--) //While testcases exist
+    {
+        int n;
+        cin>>n; //Input size of array
+        
+        int arr[n]; //Array of size n
+        
+        for(int i = 0; i < n; i++)
+        {
+            cin>>arr[i]; //input elements of array
+        }
+        Solution ob;
+        
+        cout << ob.minAdjDiff(arr, n) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
